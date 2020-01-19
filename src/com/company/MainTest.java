4b9fd306471e45dd2208f31bad188409417889f3
rijimuge <david.russell04@gmail.com>
2020-01-19 13:13:57 -0600

    test

---
+++ src/com/company/MainTest.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DisplayName("Test of Main System output")
+class MainTest {
+  //@DynamicTest
+}
