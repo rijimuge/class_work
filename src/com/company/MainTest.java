e45db77f513bc3d022c18533fc5a3f746cd1c3bd
rijimuge <david.russell04@gmail.com>
2020-01-19 14:41:34 -0600

    test

--- src/com/company/MainTest.java
+++ src/com/company/MainTest.java
@@ -2,9 +2,14 @@ package com.company;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("Test of Main System output")
 class MainTest {
-  //@DynamicTest
+  private final boolean bool = true;
+  @Test
+  void trueTest() {
+    assertTrue(bool);
+  }
 }
